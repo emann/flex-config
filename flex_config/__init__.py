fceb6032669159d833328da0a4eb4627e13cacc9
Ethan Mann <emannguitar@gmail.com>
2020-11-13 12:31:54 -0500

    Added support for using callables that return a ConfigSource as a ConfigSource (#7)
    Co-authored-by: Ethan Mann <emann@triaxtec.com>

--- flex_config/__init__.py
+++ flex_config/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["ConfigSchema", "ConfigSource", "AWSSource", "EnvSource", "YAMLSource"]
-from typing import Any, Dict, Sequence, Type, TypeVar, Union
+from typing import Any, Callable, Dict, Sequence, Type, TypeVar, Union
 
 from pydantic import BaseModel as ConfigSchema
 
@@ -7,6 +7,8 @@ from .aws_source import AWSSource
 from .config_source import ConfigSource
 from .env_source import EnvSource
 from .yaml_source import YAMLSource
+
+_SourceTypes = Union[ConfigSource, Callable[[Dict[str, Any]], ConfigSource]]
 
 
 def _merge_sources(dest: Dict[str, Any], source: ConfigSource) -> Dict[str, Any]:
@@ -22,12 +24,14 @@ def _merge_sources(dest: Dict[str, Any], source: ConfigSource) -> Dict[str, Any]
     return dest
 
 
-def _compile_sources(sources: Union[Sequence[ConfigSource], ConfigSource]) -> Dict[str, Any]:
+def _compile_sources(sources: Union[Sequence[_SourceTypes], _SourceTypes]) -> Dict[str, Any]:
     if not isinstance(sources, Sequence):
         sources = [sources]
 
     compiled: Dict[str, Any] = {}
     for source in sources:
+        if callable(source):
+            source = source(compiled)  # Pass what we have parsed so far to the function
         compiled = _merge_sources(dest=compiled, source=source)
     return compiled
 
@@ -36,7 +40,7 @@ ConfigClass = TypeVar("ConfigClass")
 
 
 def construct_config(
-    config_schema: Type[ConfigClass], sources: Union[Sequence[ConfigSource], ConfigSource]
+    config_schema: Type[ConfigClass], sources: Union[Sequence[_SourceTypes], _SourceTypes]
 ) -> ConfigClass:
     if not isinstance(config_schema, ConfigSchema):
         raise TypeError("Config schema supplied isn't a subclass of ConfigSchema (aka Pydantic's BaseModel)")
