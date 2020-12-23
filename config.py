8851da37e848a47f08b76c8a74fe4377427f62cb
Ethan Mann <emann@triaxtec.com>
2020-12-23 13:34:55 -0500

    Deployed c38417d with MkDocs version: 1.1.2

--- config.py
+++ config.py
@@ -1,10 +1,32 @@
 from pathlib import Path
 from typing import Any, Dict, Optional
 
-from flex_config import AWSSource, EnvSource, FlexConfig, YAMLSource
+from flex_config import AWSSource, ConfigSchema, ConfigSource, EnvSource, YAMLSource, construct_config
 
-_app_config: Optional[FlexConfig] = None
-yaml_path = Path.cwd() / "config.yml"
+
+class DataBaseConfig(ConfigSchema):
+    url: str
+    username: str
+    password: str
+
+
+class SentryConfig(ConfigSchema):
+    url: str
+    traces_sample_rate: float = 0.1
+
+
+class Config(ConfigSchema):
+    env: str
+    special_number: int
+    database: DataBaseConfig
+    sentry: SentryConfig
+
+
+def get_ssm_params(config_so_far: Dict[str, Any]) -> ConfigSource:
+    env = config_so_far.get("env")
+    if env == "local":
+        return {}
+    return AWSSource(f"my_app/{env}")
 
 
 default_config = {
@@ -12,8 +34,12 @@ default_config = {
 }
 
 
-def get_config(override: Dict[str, Any] = None) -> FlexConfig:
-    """ Get the app config for this  """
+_app_config: Optional[Config] = None
+yaml_path = Path.cwd() / "config.yml"
+
+
+def get_config(override: Dict[str, Any] = None) -> Config:
+    """ Get the app config for this app """
     global _app_config
 
     if _app_config:
@@ -22,16 +48,15 @@ def get_config(override: Dict[str, Any] = None) -> FlexConfig:
     if not override:
         override = {}
 
-    _app_config = FlexConfig()
-    _app_config.load_sources([
-        default_config,
-        EnvSource("APP_"),
-        YAMLSource(yaml_path),
-        override,
-    ])
-
-    env = _app_config["env"]
-    if env != "local":
-        _app_config.load_sources(AWSSource(f"app/{env}"))
+    _app_config = construct_config(
+        config_schema=Config,
+        sources=[
+            default_config,
+            EnvSource("CAT_"),
+            YAMLSource(yaml_path),
+            get_ssm_params,
+            override,
+        ],
+    )
 
     return _app_config
